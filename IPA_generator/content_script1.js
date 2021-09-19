c49e4dc1f079178867764fce3f4fa65fdc4b41ff
Parikshit Hooda <phooda804@live.com>
2021-09-20 01:37:28 +0530

    success content_script1.js inject based on current tab url

--- IPA_generator/content_script1.js
+++ IPA_generator/content_script1.js
@@ -9,3 +9,10 @@
     // console.log('teststr');
 
 console.log('content script loaded'); //sucessfully loaded
+
+document.getElementById("findipabtn").addEventListener("click", function(){
+  var sourceData = document.querySelector('[aria-label="Source text"]').nextElementSibling.innerHTML;
+  var targetData = document.querySelector('[lang="fr"]').childNodes[0].innerText;
+  console.log('source data ' + sourceData);
+  console.log('target data ' + targetData);
+})
--- IPA_generator/popup/popup_test1.js
+++ IPA_generator/popup/popup_test1.js
@@ -1,3 +1,63 @@
-function findipabtnclicked(){
-  document.getElementById('tl_p').innerHTML = "Target Language: Fr";
-}
+// function findipabtnclicked(){
+//   document.getElementById('tl_p').innerHTML = "Target Language: Fr";
+// }
+//
+//
+
+// function onGot(tabinfo) {
+//   console.log(tabinfo);
+//
+//   if (tabinfo.url != "https://translate.google.com") {
+//     var popuphead = document.getElementById("popupHeading");
+//     popuphead.appendchild("<span>POPUP ONLY WORKS ON GOOGLE TRANSLATE");
+//
+//     document.body.innerHTML = "";
+//
+//
+//   } else {
+//     var popuphead = document.getElementById("popupHeading");
+//     popuphead.appendchild("<span>POPUP WORKS HERE");
+//   }
+//
+// }
+//
+// function onError(error) {
+//   console.log(`Error: ${error}`);
+// }
+//
+// const gettingCurrent = browser.tabs.getCurrent();
+// gettingCurrent.then(onGot, onError);
+
+console.log('popup_test1.js successful')
+browser.tabs.query({currentWindow: true, active: true}, tabs => {
+    var url = tabs[0].url;
+    var googleTranslateRegex = new RegExp('htt(p|ps):\/\/translate\.google\.[a-z]+');
+
+    console.log(url);
+    if(googleTranslateRegex.test(url)) { //is block works
+
+      function onExecuted(result) {
+        console.log(`content_script1 injected`); //successfully loaded
+      }
+
+      function onError(error) {
+        console.log(`Error: ${error}`);
+      }
+
+      const executing = browser.tabs.executeScript({
+        file: "/content_script1.js"
+      });
+      executing.then(onExecuted, onError); //successfuly resolution
+
+
+    } else { //this else block works
+      document.body.innerHTML = '';
+      // document.body.innerHTML("<span>This extension works on Google Translate website on Mozilla Firefox(currently).</span>")
+      console.log("url not appropriate. please use google translate.")
+      let div = document.createElement('div');
+      div.innerHTML = "<span>This extension works on Google Translate website on Mozilla Firefox(currently).</span>";
+      document.body.append(div);
+    }
+
+
+});
