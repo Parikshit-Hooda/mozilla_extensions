24c171d673e87c5644bd463d27aef72bccc65659
Parikshit Hooda <phooda804@live.com>
2021-11-18 02:35:45 +0530

    refactored comments in content_script1.js

--- IPA_generator/content_script1.js
+++ IPA_generator/content_script1.js
@@ -1,4 +1,3 @@
-
 function getSentence(lenSentenceNodes, nodeId) {
   let numNodes = lenSentenceNodes - 1;
   let resText = "";
@@ -6,44 +5,27 @@ function getSentence(lenSentenceNodes, nodeId) {
   for(let i = 0 ; i < numNodes; i = i+1) {
     resText = resText + document.querySelectorAll("[data-language=" + getStateAtLargeWidthResult[nodeId + "-data-language-code"] + "] > div > span > span > span")[i].textContent;
   }
-
   return resText;
-  // document.querySelectorAll("[data-language=" + getStateAtLargeWidthResult["i12-data-language-code"] + "] > div > span > span > span")[0].textContent;
-
-  /*
-  var tgtTextListLength = document.querySelectorAll("[data-language=fr] > div > span > span > span").length
-  var tgtText = "";
-  console.log(tgtTextListLength);
-  for(var i = 0 ; i < tgtTextListLength; i = i+1) {
-    tgtText = tgtText + document.querySelectorAll("[data-language=fr] > div > span > span > span")[i].textContent
-  */
 }
 
 let getStateAtLargeWidthResult = {};
 
-function getStateAtLargeWidth() {
+function getStateAtLargeWidth() { // this functions captures sate of Google Transalte wesbtie when the window view px >= 720ps
   //first check for screen size
   let resultState = {};
   //fact recognized - for > 720px innerWidth, the three tab layout manifests
 
   //consider using window.innerWidth || document.documentElement || document.documentElement('body')[0].clientHeight
-  const webPageWidth = (window.innerWidth || document.documentElement.clientWidth || document.documentElement('body')[0].clientHeight);
+  const webPageWidth = (window.innerWidth || document.documentElement.clientWidth || document.documentElement('body')[0].clientHeight); //find web page width - exclusive of Developer console if opened
   console.log("content_script1.js webPageWidth " + webPageWidth); //web page width fetch successful
 
   if(webPageWidth>=720) {
-
     console.log("called when screen width >= 720 px");
-
     resultState["pageWidthFlag"] = "1"; //flag to identify current page width
-
-    //      below object fields get information for left panel on Google Translate
 
     //-TODO : enhancement: for source language= "detect language" tab, use [data-language-code] = auto to find the correct button id first, then find values of attributes
 
-    // resultState["i8-aria-selected"] = document.getElementById("i8").getAttribute("aria-selected");
-    // resultState["i8-tabIndex"] = document.getElementById("i8").getAttribute("tabIndex");
-    // resultState["i8-data-language-code"] = document.getElementById("i8").getAttribute("data-language-code");
-
+    //below object fields get information for left panel(source language) on Google Translate
     resultState["i9-aria-selected"] = document.getElementById("i9").getAttribute("aria-selected");
     resultState["i9-tabIndex"] = document.getElementById("i9").getAttribute("tabIndex");
     resultState["i9-data-language-code"] = document.getElementById("i9").getAttribute("data-language-code");
@@ -56,7 +38,7 @@ function getStateAtLargeWidth() {
     resultState["i11-tabIndex"] = document.getElementById("i11").getAttribute("tabIndex");
     resultState["i11-data-language-code"] = document.getElementById("i11").getAttribute("data-language-code");
 
-    //below object fields get information for right panel on Google Translate
+    //below object fields get information for right panel(target language) on Google Translate
     resultState["i12-aria-selected"] = document.getElementById("i12").getAttribute("aria-selected");
     resultState["i12-tabIndex"] = document.getElementById("i12").getAttribute("tabIndex");
     resultState["i12-data-language-code"] = document.getElementById("i12").getAttribute("data-language-code");
@@ -69,17 +51,16 @@ function getStateAtLargeWidth() {
     resultState["i14-tabIndex"] = document.getElementById("i14").getAttribute("tabIndex");
     resultState["i14-data-language-code"] = document.getElementById("i14").getAttribute("data-language-code");
 
-  }
-  else {
-    console.log("called when screen width < 720 px");
-    resultState["pageWidthFlag"] = "0";
-  }
+    }
+    else {
+      console.log("called when screen width < 720 px");
+      resultState["pageWidthFlag"] = "0";
+    }
 
   console.log("from getStateAtLargeWidth");
   console.log("logging resultState object" + JSON.stringify(resultState));
 
   return resultState;
-
 }
 
 browser.runtime.onMessage.addListener(request => {
@@ -88,16 +69,17 @@ browser.runtime.onMessage.addListener(request => {
   console.log("content_script1.js - " + request.messageContent); //log success
 
   let messageType;
-  let sourceText = "";
-  let targetText = "";
+  let sourceText = ""; //stores text from left panel
+  let targetText = ""; //stores text from right panel
 
   if(request.messageContent == "Find IPA button clicked") {
     messageType = 1;
     //find current Google Translate site state
 
-    console.log("content_Script1.js - messageType = 1 if block executed");
+    console.log("content_Script1.js - Find IPA");
 
-    //'getting' works only for large width as of now
+    //'getting' works only for large width as of now.
+    //Enhancement: work for middle and small webPageWidth too
     getStateAtLargeWidthResult = getStateAtLargeWidth();
 
     //var currentTarget;
@@ -111,6 +93,7 @@ browser.runtime.onMessage.addListener(request => {
     // console.log("sourceText variable populated.");
     getStateAtLargeWidthResult["sourceText"] = document.querySelector('[aria-label="Source text"]').nextElementSibling.innerHTML;
 
+    //get target text
     if (getStateAtLargeWidthResult["i12-aria-selected"] == "true") {
       let sentenceElementsSize = document.querySelectorAll("[data-language=" + getStateAtLargeWidthResult["i12-data-language-code"] + "] > div > span > span > span").length;
       // targetText = getSentence(size, i12);
@@ -139,22 +122,18 @@ browser.runtime.onMessage.addListener(request => {
 
     getStateAtLargeWidthResult["targetText"] = targetText;
 
+    //below log checks the source and target text to see if input received properly or not. Comment if in production
     console.log("content_Script1.js source text " + sourceText);
     console.log("content_Script1.js target text " + targetText);
 
     console.log("content_script1.js getStateAtLargeWidthResult " + JSON.stringify(getStateAtLargeWidthResult)); //to check the content of getStateAtLargeWidthResult variable
-
     //TODO: add for stateAtMediumWidth and stateAtSmallWidth
-
-  }
-
-
-} else {
+    }
+  } else {
     messageType = 0;
     getStateAtLargeWidthResult["sourceText"] = "invalid";
     getStateAtLargeWidthResult["targetText"] = "invalid";
-    console.log("content_Script1.js - messageType = 0 else block executed");
-
+    console.log("content_Script1.js - messageType = 0. Invalid input. Check input languages. Keep then French or English.");
   }
 
   return Promise.resolve({response: "From content script", messageType: messageType, responseObj: getStateAtLargeWidthResult}); //successful resolution
